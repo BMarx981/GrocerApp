1ec7e1a71cc4a61a1eddec755291790fc960c095
BMarx981 <b.marx981@gmail.com>
2024-02-02 14:51:02 -0500

    Create database with working connection.

--- lib/data/source/database/database.dart
+++ lib/data/source/database/database.dart
@@ -2,9 +2,14 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:logger/logger.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:logger/logger.dart';
+// import 'package:sqlite3/sqlite3.dart';
+// import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 part 'database.g.dart';
 
@@ -36,19 +41,24 @@ class RecipeGroceryItems extends Table {
 
 @DriftDatabase(tables: [GroceryItems, RecipeGroceryItems, Recipes])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase(super.e);
+  AppDatabase() : super(_openConnection());
 
   @override
   int get schemaVersion => 3;
+}
 
-  LazyDatabase _openConnection() {
-    final logger = Logger();
-    return LazyDatabase(() async {
-      final dbFolder = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dbFolder.path, 'db.sqlite'));
-      logger.t("Database connection opening");
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
 
-      return NativeDatabase.createInBackground(file);
-    });
-  }
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+
+    final cachebase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cachebase;
+
+    return NativeDatabase.createInBackground(file);
+  });
 }
